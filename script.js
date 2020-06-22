9da208f3afa4bddc92abbc2ea33ceacacfffc468
Dominik <rusac.dominik@gmail.com>
2020-06-22 17:16:20 +0200

    difficulty setting

--- script.js
+++ script.js
@@ -41,6 +41,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Set difficulty to value in ls or medium
+let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+// Set difficulty select value
+difficultySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
 // Focus on text on start
 text.focus();
 
@@ -69,7 +75,7 @@ function updateTime() {
     time--;
     timeEl.innerHTML = time + 's';
 
-    if(time === 0) {
+    if (time === 0) {
         clearInterval(timeInterval);
         // end game
         gameOver();
@@ -90,6 +96,8 @@ function gameOver() {
 addWordToDOM();
 
 // Event listeners
+
+// Typing
 text.addEventListener('input', e => {
     const insertedText = e.target.value;
 
@@ -100,8 +108,23 @@ text.addEventListener('input', e => {
         // Clear
         e.target.value = '';
 
-        time += 5;
+        if (difficulty === 'hard') {
+            time += 2;
+        } else if (difficulty === 'medium') {
+            time += 3;
+        } else {
+            time += 5;
+        }
 
         updateTime();
     }
-})
+});
+
+// Settings btn click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+// Settings select
+settingsForm.addEventListener('change', e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
+});
